f0628208fbcfa861252c90255b1d4f98699a276b
Shubhojeet Bera <shubhobera98@gmail.com>
2023-01-26 13:32:08 +0530

    price added on first row

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,16 @@
 import React, {useState} from "react";
 import "../App.css";
+import img1 from "./img/nike1.png";
+import img2 from "./img/nike2.png";
+import img3 from "./img/nike3.png";
+import img4 from "./img/nike4.png";
 import Products from "./Products";
 
 export default function Body() {
 
 
   const [Nikename, setNikename] = useState("Nike Shoe")
+  const [Pumaname, setPumaname] = useState("Puma Shoe")
 
   return (
     <>
@@ -29,16 +34,16 @@ export default function Body() {
         </div>
         <div className="products d-flex justify-content-center my-5 row">
           <div className="first-row row d-flex justify-content-center">
-          <Products title={Nikename + " " + 1}/>
-          <Products title={Nikename + " " + 2}/>
-          <Products title={Nikename + " " + 3}/>
-          <Products title={Nikename + " " + 4}/>
+          <Products image={img1} title={Nikename + " " + 1} price1="1,999" price2="899"/>
+          <Products image={img2} title={Nikename + " " + 2} price1="1,999" price2="899"/>
+          <Products image={img3} title={Nikename + " " + 3} price1="1,999" price2="899"/>
+          <Products image={img4} title={Nikename + " " + 4} price1="1,999" price2="899"/>
           </div>
           <div className="second-row row my-4 mb-0 d-flex justify-content-center">
-          <Products title={Nikename + " " + 5}/>
-          <Products title={Nikename + " " + 6}/>
-          <Products title={Nikename + " " + 7}/>
-          <Products title={Nikename + " " + 8}/>
+          <Products title={Pumaname + " " + 1}/>
+          <Products title={Pumaname + " " + 2}/>
+          <Products title={Pumaname + " " + 3}/>
+          <Products title={Pumaname + " " + 4}/>
           </div>
           <div className="third-row row my-4 d-flex justify-content-center">
           <Products title={Nikename + " " + 9}/>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,5 @@
 import "../App.css";
 import React from 'react'
-import img1 from "./img/nike1.png";
 import rating from "./img/rating.png";
 
 export default function Products(props) {
@@ -8,7 +7,7 @@ export default function Products(props) {
     <>
     <div class="card col-sm mx-2 my-2 mb-0" style={{ maxWidth: "250px" }}>
             <img
-              src={img1}
+              src={props.image}
               class="card-img-top"
               alt="..."
               style={{ padding: "25px", backgroundColor: "#F7F7F7" }}
@@ -27,9 +26,14 @@ export default function Products(props) {
                 <div className="reviews mx-1">(123 reviews)</div>
                 </div>
               </p>
+              <div className="purchase d-flex justify-content-between">
+                <div className="price1" style={{textDecoration:"line-through", fontSize:"20px"}}>₹{props.price1}</div>
+                <div className="price2" style={{fontSize:"20px"}}>₹{props.price2}</div>
               <a href="/" class="btn btn-primary">
-                Buy
+                Cart
               </a>
+              
+              </div>
             </div>
           </div>
     </>
