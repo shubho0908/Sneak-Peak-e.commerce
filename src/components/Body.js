104bc23a9d2224042e625551b992d27aba3b4041
Shubhojeet Bera <shubhobera98@gmail.com>
2023-01-28 12:38:33 +0530

    updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../App.css";
 import img1 from "./img/nike1.png";
 import img2 from "./img/nike2.png";
@@ -19,132 +19,181 @@ import img16 from "./img/reebook4.png";
 import Products from "./Products";
 
 export default function Body() {
-
-
-  const [Nikename, setNikename] = useState("Nike Shoe")
-  const [Pumaname, setPumaname] = useState("Puma Shoe")
-  const [AdidasName, setAdidasName] = useState("Adidas Shoe")
-  const [ReebookName, setReebookName] = useState("Reebook Shoe")
-
-  const Active1=()=>{
-    document.getElementById('all').classList.add("active")
-    document.getElementById('nike').classList.remove("active")
-    document.getElementById('puma').classList.remove("active")
-    document.getElementById('reebook').classList.remove("active")
-    document.getElementById('adidas').classList.remove("active")
-
-
-    document.getElementById('nike1').style.setProperty('display', 'flex', 'important');
-    document.getElementById('adidas1').style.setProperty('display', 'flex', 'important');
-    document.getElementById('puma1').style.setProperty('display', 'flex', 'important');
-    document.getElementById('reebook1').style.setProperty('display', 'flex', 'important');
-
-  }
-  
-  const Active2=()=>{
-    document.getElementById('nike').classList.add("active")
-    document.getElementById('all').classList.remove("active")
-    document.getElementById('puma').classList.remove("active")
-    document.getElementById('reebook').classList.remove("active")
-    document.getElementById('adidas').classList.remove("active")
-    
-    
-    document.getElementById('puma1').style.setProperty('display', 'none', 'important');
-    document.getElementById('adidas1').style.setProperty('display', 'none', 'important');
-    document.getElementById('nike1').style.setProperty('display', 'flex', 'important');
-    
-
-  }
-
-  const Active3=()=>{
-    document.getElementById('nike').classList.remove("active")
-    document.getElementById('all').classList.remove("active")
-    document.getElementById('puma').classList.remove("active")
-    document.getElementById('reebook').classList.remove("active")
-    document.getElementById('adidas').classList.add("active")
-
-    document.getElementById('nike1').style.setProperty('display', 'none', 'important');
-    document.getElementById('puma1').style.setProperty('display', 'none', 'important');
-    document.getElementById('reebook1').style.setProperty('display', 'none', 'important');
-    document.getElementById('adidas1').style.setProperty('display', 'flex', 'important');
-
-  }
-
-  const Active4=()=>{
-    document.getElementById('nike').classList.remove("active")
-    document.getElementById('all').classList.remove("active")
-    document.getElementById('puma').classList.add("active")
-    document.getElementById('reebook').classList.remove("active")
-    document.getElementById('adidas').classList.remove("active")
-
-    document.getElementById('nike1').style.setProperty('display', 'none', 'important');
-    document.getElementById('reebook1').style.setProperty('display', 'none', 'important');
-    document.getElementById('adidas1').style.setProperty('display', 'none', 'important');
-    document.getElementById('puma1').style.setProperty('display', 'flex', 'important');
-
-
-  }
-
-  const Active5=()=>{
-    document.getElementById('nike').classList.remove("active")
-    document.getElementById('all').classList.remove("active")
-    document.getElementById('puma').classList.remove("active")
-    document.getElementById('reebook').classList.add("active")
-    document.getElementById('adidas').classList.remove("active")
-
-    document.getElementById('nike1').style.setProperty('display', 'none', 'important');
-    document.getElementById('reebook1').style.setProperty('display', 'flex', 'important');
-    document.getElementById('adidas1').style.setProperty('display', 'none', 'important');
-    document.getElementById('puma1').style.setProperty('display', 'none', 'important');
-  }
-
-
-  
+  const [Nikename, setNikename] = useState("Nike Shoe");
+  const [Pumaname, setPumaname] = useState("Puma Shoe");
+  const [AdidasName, setAdidasName] = useState("Adidas Shoe");
+  const [ReebookName, setReebookName] = useState("Reebook Shoe");
 
   return (
     <>
       <div className="container my-5 ">
         <div className="lists d-flex flex-wrap justify-content-center my-1">
-          <button id="all" onClick={Active1} type="button" class="btn btn-outline-primary active mx-3">
-            All Products
-          </button>
-          <button id="nike" onClick={Active2} type="button" class="btn btn-outline-primary mx-3">
-            Nike
-          </button>
-          <button id="adidas" onClick={Active3} type="button" class="btn btn-outline-primary mx-3">
-            Adidas
-          </button>
-          <button id="puma" onClick={Active4} type="button" class="btn btn-outline-primary mx-3">
-            Puma
-          </button>
-          <button id="reebook" onClick={Active5} type="button" class="btn btn-outline-primary mx-3">
-            Reebook
-          </button>
+          <h2>Have a Sneak-Peak</h2>
         </div>
         <div className="products d-flex justify-content-center my-5 row">
-          <div id="nike1" className="first-row row d-flex justify-content-center ">
-          <Products image={img1} image1={img1} title={Nikename + " " + 1}  title1={Nikename + " " + 1} price1="1,999" price2="899" cart="one"/>
-          <Products image={img2} image2={img2} title={Nikename + " " + 2} title2={Nikename + " " + 2} price1="1,999" price2="899" cart="two"/>
-          <Products image={img3} image3={img3} title={Nikename + " " + 3} title3={Nikename + " " + 3} price1="1,999" price2="899" cart="three"/>
-          <Products image={img4} image4={img4} title={Nikename + " " + 4} title4={Nikename + " " + 4} price1="1,999" price2="899" cart="four"/>
+          <div
+            id="nike1"
+            className="first-row row d-flex justify-content-center "
+          >
+            <Products
+              image={img1}
+              image1={img1}
+              title={Nikename + " " + 1}
+              title1={Nikename + " " + 1}
+              price1="1,999"
+              price2="899"
+              cart="one"
+            />
+            <Products
+              image={img2}
+              image2={img2}
+              title={Nikename + " " + 2}
+              title2={Nikename + " " + 2}
+              price1="1,999"
+              price2="899"
+              cart="two"
+            />
+            <Products
+              image={img3}
+              image3={img3}
+              title={Nikename + " " + 3}
+              title3={Nikename + " " + 3}
+              price1="1,999"
+              price2="899"
+              cart="three"
+            />
+            <Products
+              image={img4}
+              image4={img4}
+              title={Nikename + " " + 4}
+              title4={Nikename + " " + 4}
+              price1="1,999"
+              price2="899"
+              cart="four"
+            />
           </div>
-          <div id="puma1" className="second-row row mb-0 d-flex justify-content-center ">
-          <Products image={img5} image5={img5} title={Pumaname + " " + 1} title5={Pumaname + " " + 1} price1="2,499" price2="999" cart="five"/>
-          <Products image={img6} image6={img6} title={Pumaname + " " + 2} title6={Pumaname + " " + 2} price1="2,499" price2="999" cart="six"/>
-          <Products image={img7} image7={img7} title={Pumaname + " " + 3} title7={Pumaname + " " + 3} price1="2,499" price2="999" cart="seven"/>
-          <Products image={img8} image8={img8} title={Pumaname + " " + 4} title8={Pumaname + " " + 4} price1="2,499" price2="999" cart="eight"/>
+          <div
+            id="puma1"
+            className="second-row row mb-0 d-flex justify-content-center "
+          >
+            <Products
+              image={img5}
+              image5={img5}
+              title={Pumaname + " " + 1}
+              title5={Pumaname + " " + 1}
+              price1="2,499"
+              price2="999"
+              cart="five"
+            />
+            <Products
+              image={img6}
+              image6={img6}
+              title={Pumaname + " " + 2}
+              title6={Pumaname + " " + 2}
+              price1="2,499"
+              price2="999"
+              cart="six"
+            />
+            <Products
+              image={img7}
+              image7={img7}
+              title={Pumaname + " " + 3}
+              title7={Pumaname + " " + 3}
+              price1="2,499"
+              price2="999"
+              cart="seven"
+            />
+            <Products
+              image={img8}
+              image8={img8}
+              title={Pumaname + " " + 4}
+              title8={Pumaname + " " + 4}
+              price1="2,499"
+              price2="999"
+              cart="eight"
+            />
           </div>
-          <div id="adidas1" className="third-row row d-flex justify-content-center ">
-          <Products image={img9} image9={img9} title={AdidasName + " " + 1} title9={AdidasName + " " + 1} price1="1,499" price2="799" cart="nine"/>
-          <Products image={img10} image10={img10} title={AdidasName + " " + 2} title10={AdidasName + " " + 2} price1="1,499" price2="799" cart="ten"/>
-          <Products image={img11} image11={img11} title={AdidasName + " " + 3} title11={AdidasName + " " + 3} price1="1,499" price2="799" cart="eleven"/>
-          <Products image={img12} image12={img12} title={AdidasName + " " + 4} title12={AdidasName + " " + 4} price1="1,499" price2="799" cart="twelve"/>
+          <div
+            id="adidas1"
+            className="third-row row d-flex justify-content-center "
+          >
+            <Products
+              image={img9}
+              image9={img9}
+              title={AdidasName + " " + 1}
+              title9={AdidasName + " " + 1}
+              price1="1,499"
+              price2="799"
+              cart="nine"
+            />
+            <Products
+              image={img10}
+              image10={img10}
+              title={AdidasName + " " + 2}
+              title10={AdidasName + " " + 2}
+              price1="1,499"
+              price2="799"
+              cart="ten"
+            />
+            <Products
+              image={img11}
+              image11={img11}
+              title={AdidasName + " " + 3}
+              title11={AdidasName + " " + 3}
+              price1="1,499"
+              price2="799"
+              cart="eleven"
+            />
+            <Products
+              image={img12}
+              image12={img12}
+              title={AdidasName + " " + 4}
+              title12={AdidasName + " " + 4}
+              price1="1,499"
+              price2="799"
+              cart="twelve"
+            />
           </div>
-          <div id="reebook1" className="fourth-row row d-flex justify-content-center ">
-          <Products image={img13} image13={img13} title={ReebookName + " " + 1} title13={ReebookName + " " + 1} price1="1,999" price2="899" cart="thirteen"/>
-          <Products image={img14} image14={img14} title={ReebookName + " " + 2} title14={ReebookName + " " + 2} price1="1,999" price2="899" cart="fourteen"/>
-          <Products image={img15} image15={img15} title={ReebookName + " " + 3} title15={ReebookName + " " + 3} price1="1,999" price2="899" cart="fifteen"/>
-          <Products image={img16} image16={img16} title={ReebookName + " " + 4} title16={ReebookName + " " + 4} price1="1,999" price2="899" cart="sixteen"/>
+          <div
+            id="reebook1"
+            className="fourth-row row d-flex justify-content-center "
+          >
+            <Products
+              image={img13}
+              image13={img13}
+              title={ReebookName + " " + 1}
+              title13={ReebookName + " " + 1}
+              price1="1,999"
+              price2="899"
+              cart="thirteen"
+            />
+            <Products
+              image={img14}
+              image14={img14}
+              title={ReebookName + " " + 2}
+              title14={ReebookName + " " + 2}
+              price1="1,999"
+              price2="899"
+              cart="fourteen"
+            />
+            <Products
+              image={img15}
+              image15={img15}
+              title={ReebookName + " " + 3}
+              title15={ReebookName + " " + 3}
+              price1="1,999"
+              price2="899"
+              cart="fifteen"
+            />
+            <Products
+              image={img16}
+              image16={img16}
+              title={ReebookName + " " + 4}
+              title16={ReebookName + " " + 4}
+              price1="1,999"
+              price2="899"
+              cart="sixteen"
+            />
           </div>
         </div>
       </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -14,12 +14,14 @@ export default function Products(props) {
     if (props.cart === "one") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image1} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image1} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title1}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
               
             </div>
       `
@@ -27,180 +29,209 @@ export default function Products(props) {
     else if (props.cart === "two") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image2} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image2} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title2}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "three") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image3} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image3} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title3}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "four") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image4} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image4} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title4}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "five") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image5} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image5} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title5}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "six") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image6} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image6} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title6}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "seven") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image7} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image7} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title7}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "eight") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image8} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image8} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title8}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "nine") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image9} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image9} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title9}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "ten") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image10} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image10} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title10}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "eleven") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image11} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image11} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title11}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "twelve") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image12} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image12} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title12}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "thirteen") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image13} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image13} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title13}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "fourteen") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image14} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image14} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title14}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
     else if (props.cart === "fifteen") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image15} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image15} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title15}</h5>
               
-            </div>
+              </div>
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
       `
     }
     else if (props.cart === "sixteen") {
       document.getElementById('cartBody').innerHTML = 
       `
-            <img src=${props.image16} class="img-fluid rounded-start" alt="..." />
+            <img src=${props.image16} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">${props.title16}</h5>
               
+              
+              <h5 class="price card-title my-2">₹${props.price2}</h5>
             </div>
       `
     }
@@ -217,7 +248,7 @@ export default function Products(props) {
           alt="..."
           style={{ padding: "25px", backgroundColor: "#F7F7F7" }}
         />
-        <div class="card-body">
+        <div class="card-body mx-2">
           <h5 class="card-title">{props.title}</h5>
           <p class="card-text d-flex">
             <img
@@ -274,14 +305,16 @@ export default function Products(props) {
           ></button>
         </div>
         <div class="offcanvas-body" style={{margin:"10px"}}>
-           <div id="cartBody" class="col-md-4 mx-2">
-           {/* <img src={props.image} class="img-fluid rounded-start" alt="..." />
+           <div className="cartData">
+           <div id="cartBody" class="col-md-4 my-3 mx-2">
+           {/* <img src={props.image} class="img-fluid rounded-start productImg" alt="..." />
           </div>
           <div class="col-md-8 mx-2">
             <div class="card-body">
               <h5 class="card-title">{props.title}</h5>
               
             </div>*/}
+           </div>
           </div> 
         </div>
       </div>
